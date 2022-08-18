fc69730047fa8cfd9fc276d672502b6b8e5966e1
shitikyan <shitikyan@gmail.com>
2022-08-18 16:05:21 +0400

    Added main login

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import json
+
 from fastapi import FastAPI
 from pydantic import BaseModel
 import requests
@@ -11,30 +13,28 @@ class RasaRequest(BaseModel):
 class RasaResponse(BaseModel):
     recipient_id: str
     text: str
+    is_correct: bool
 
 
 app = FastAPI()
 rasaUrl = "http://rasa:5005/webhooks/rest/webhook"
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
 
 @app.post("/api/")
 async def api(request: RasaRequest):
-    pass
+    response = parse_user_question(request)
+    return response
 
 
-def toRasa(request: RasaRequest):
-    # requests.post(rasaUrl, )
-    return [1, 2]
+def to_rasa(request: RasaRequest):
+    r = requests.post(rasaUrl, data=request.json())
+    res_obj = json.loads(r.text)
+    response: RasaResponse = RasaResponse(recipient_id=res_obj['recipient_id'], text=res_obj['text'], is_correct=res_obj['correct'])
+
+    return response
 
 
-def parseUserQuestion(body: RasaRequest):
-    isCorrect = True
+def parse_user_question(body: RasaRequest):
+    response = to_rasa(body)
 
-    response, err = toRasa(body)
-
-    if response.text == "":
-        isCorrect = False
-
+    return response
